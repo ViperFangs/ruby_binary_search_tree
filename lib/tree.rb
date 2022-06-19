8a0be8421458513aba86633017e0df3a24732721
Aarya <amathrej@asu.edu>
2022-06-19 03:54:50 -0700

    Implement #build_tree in class Tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -6,11 +6,21 @@ require_relative 'node'
 class Tree
   attr_accessor :root
 
-  def initialize
-    self.root = build_tree
+  def initialize(array)
+    self.root = build_tree(array)
   end
 
-  def build_tree
-    return
+  def build_tree(array, start_index = 0, end_index = (array.length - 1))
+    return nil if start_index > end_index
+
+    mid_index = (start_index + end_index) / 2
+    new_node = Node.new(array[mid_index])
+
+    new_node.right = build_tree(array, start_index, mid_index - 1)
+    new_node.left = build_tree(array, mid_index + 1, end_index)
+
+    new_node
   end
 end
+
+# Tree.new([1, 2, 3, 4])
