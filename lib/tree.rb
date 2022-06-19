5eef6da3efb72006ffa13b5256533082f999ea19
Aarya <amathrej@asu.edu>
2022-06-19 03:40:20 -0700

    Add #initiliaze and #build_tree for class Tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal: true
 
 require_relative 'node'
+
+# class Tree accepts an array when initialized and has a root attribute
+class Tree
+  attr_accessor :root
+
+  def initialize
+    self.root = build_tree
+  end
+
+  def build_tree
+    return
+  end
+end
