8a12d6d80d6bd47c7141b2ec47a5e312b62c2292
Aarya <amathrej@asu.edu>
2022-06-19 03:36:19 -0700

    Mixin with Comparable module

--- lib/node.rb
+++ lib/node.rb
@@ -1,5 +1,11 @@
 # class Node contains data, left and right attributes
 class Node
+  include Comparable
+
+  def <=>(other)
+    data <=> other.data
+  end
+
   attr_accessor :data, :left, :right
 
   def initialize
